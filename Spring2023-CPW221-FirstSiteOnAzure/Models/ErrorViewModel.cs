ffabaeded4e2f3831cd62a741d68123a7fb7ac98
theZalmanian <thezalmanian@gmail.com>
2023-06-08 08:19:40 -0700

    Add project files.

---
+++ Spring2023-CPW221-FirstSiteOnAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spring2023_CPW221_FirstSiteOnAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
